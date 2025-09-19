8952d5927a881b5ba4c7d6b2abb8ed6f677c80dd
Micor <63491674+Micor-byte@users.noreply.github.com>
2025-09-19 08:59:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -188,9 +188,9 @@ const addCartToHTML = () => {
                 <div class="name">${info.name}</div>
                 <div class="price info">RM${info.price}</div>
                 <div class="quantity">
-                    <span class="minus">minus</span>
+                    <span class="minus">-</span>
                     <span>${item.quantity}</span>
-                   <span class="plus">add</span>
+                   <span class="plus">+</span>
                 </div>
             `;
             listCartHTML.appendChild(newItem);
