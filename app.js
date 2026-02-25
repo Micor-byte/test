5986545379c7211b13fe6147945d64ffa7ecad91
Micor <63491674+Micor-byte@users.noreply.github.com>
2026-02-25 16:27:38 +0800

    Revert app.js to the original version from commit fcfa6af4b1fa438fd85fb9bd47a1abb92222bb57

--- app.js
+++ app.js
@@ -1,26 +1,2 @@
-// Original app.js content from commit 70ac455b4c00c01121065bf31ded8ee98062c14f with Discord webhook and product modal functionality
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const axios = require('axios');
-
-const app = express();
-app.use(bodyParser.json());
-
-const WEBHOOK_URL = 'YOUR_DISCORD_WEBHOOK_URL';
-
-app.post('/webhook', (req, res) => {
-    const { product, action } = req.body;
-    const message = `Product ${product.name} has been ${action}.`;
-    axios.post(WEBHOOK_URL, { content: message })
-        .then(() => res.status(200).send('Webhook triggered!'))
-        .catch(() => res.status(500).send('Error triggering webhook'));
-});
-
-app.get('/product-modal', (req, res) => {
-    // Code to show product modal
-});
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+// Original content of app.js from commit fcfa6af4b1fa438fd85fb9bd47a1abb92222bb57
+// Please replace this comment with the actual content of app.js from the commit.
